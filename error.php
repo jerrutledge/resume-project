fb63515fe4036f145c4ca7104f565ce95623c6e2
Jeremy Rutledge <jerrutledge@gmail.com>
2015-04-23 22:57:37 -0400

    Added support for more robust error messages

--- error.php
+++ error.php
@@ -4,10 +4,18 @@ include "html_header.php";
 ?>
 	<section class="container">
 		<h1>Error</h1>
+		<?php
+if (isset($error)) {
+	echo $error;
+} else {
+		?>
 		<p>Something went wrong. Either your browser doesn't have cookies set or your session expired. </p>
+		<?php
+}
+?>
 		<p>Try <a href="login.html">logging in</a> again.</p>
 	</section>
-	<script>	
+	<script>
 		var navigation = responsiveNav(".nav-collapse", {
 			customToggle: "#toggle"
 		});
--- login.php
+++ login.php
@@ -5,10 +5,9 @@
 
 	// Check connection
 	if (mysqli_connect_errno()) { 
-	?>
-		<p class="errortitle">Failed to connect to MySQL Database. </p>
-		<p class="small">Error info: <?php echo mysqli_connect_error(); ?></p>
-	<?php
+		$error = '<p class="errortitle">Failed to connect to MySQL Database. </p><p class="small">Error info: <?php echo mysqli_connect_error(); ?></p>';
+		include "error.php";
+		exit();
 		$success = false;
 	}
 
