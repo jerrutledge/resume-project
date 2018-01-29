6e0e7d06a5be00ff364274488f3e33d062f73dcb
Jeremy Rutledge <jerrutledge@gmail.com>
2018-01-29 15:54:50 -0500

    Prevent private access

---
+++ private/404.php
@@ -0,0 +1,6 @@
+<?php 
+$title = "Error";
+$basepath = "/../";
+$error = '<p>You have incorrectly entered the url. If you arrived here by accident, click the back button on your browser, or <a href="/login">log in again here</a>.</p>';
+
+include 'error.php';
